117a5dcbec2c4faab46fc5511ead8ff3ca90fb45
Piotr <piotr.mikos@gmail.com>
2021-09-25 11:06:28 +0200

    chore: init project

---
+++ src/main/java/pl/sdaacademy/pokemonacademyapl/PokemonAcademyAplApplication.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.pokemonacademyapl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonAcademyAplApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokemonAcademyAplApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/sdaacademy/pokemonacademyapl/PokemonAcademyAplApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sdaacademy.pokemonacademyapl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonAcademyAplApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
